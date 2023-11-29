7d1fe3ff1816264ff03d6946551e42f0d4f49e54
enolgor <enolgor@teleco.upv.es>
2023-11-29 12:15:17 +0100

    add oneof validator

--- validators/strings.go
+++ validators/strings.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"net/mail"
+	"slices"
+	"strings"
 )
 
 func (*stringValidators) NotEmpty(s *string) error {
@@ -31,6 +33,18 @@ func (*stringValidators) Len(sl int) func(s *string) error {
 		}
 		if len(*s) != sl {
 			return fmt.Errorf("string length must be %d", sl)
+		}
+		return nil
+	}
+}
+
+func (*stringValidators) OneOf(valid ...string) func(s *string) error {
+	return func(s *string) error {
+		if err := isPresent(s); err != nil {
+			return err
+		}
+		if !slices.Contains(valid, *s) {
+			return fmt.Errorf("string should be one of: [%s]", strings.Join(valid, ","))
 		}
 		return nil
 	}
